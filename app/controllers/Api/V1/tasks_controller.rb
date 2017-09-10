091d5a7fe00c3b4044a0496557adf81f645c0c60
github username <github email address>
2017-09-09 21:13:02 -0400

    all crud

--- app/controllers/Api/V1/tasks_controller.rb
+++ app/controllers/Api/V1/tasks_controller.rb
@@ -16,19 +16,19 @@ class Api::V1::TasksController < ApplicationController
     task = Task.find_by(id: params[:id])
     render json: task
   end
-  #
-  # def update
-  #   task = Task.find_by(id: params[:id])
-  #   task.update(task_params)
-  #   render json: task
-  # end
-  #
-  # def destroy
-  #   task = Task.find_by(id: params[:id])
-  #   task.destroy
-  #   tasks = Task.all
-  #   render json: tasks
-  # end
+
+  def update
+    task = Task.find_by(id: params[:id])
+    task.update(task_params)
+    render json: task
+  end
+  
+  def destroy
+    task = Task.find_by(id: params[:id])
+    task.destroy
+    tasks = Task.all
+    render json: tasks
+  end
 
   private
 
