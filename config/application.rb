548829f24ec29a39221f42153fbcfa2e8fbe6aec
github username <github email address>
2017-10-18 14:35:31 -0400

    rack cors deleted

--- config/application.rb
+++ config/application.rb
@@ -6,14 +6,10 @@ Bundler.require(*Rails.groups)
 
 module AsanaCloneBE
   class Application < Rails::Application
-    config.load_defaults 5.1
 
-      config.middleware.insert_before 0, Rack::Cors do
-            allow do
-           origins '*'
-           resource '*', :headers => :any, :methods => [:get, :post, :delete, :patch, :options]
-         end
-       end
+   config.load_defaults 5.1
+
+   config.api_only = true
 
   end
 end
