6cbec88e794190c8a3e921110b58a3fe4a29df1c
github username <github email address>
2017-11-02 14:58:59 -0400

    rack cors

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,13 @@ module AsanaCloneBE
 
    config.load_defaults 5.1
 
-   config.api_only = true
+   config.middleware.insert_before 0, Rack::Cors do
+            allow do
+           origins '*'
+           resource '*', :headers => :any, :methods => [:get, :post, :delete, :patch, :options]
+         end
+       end
+    config.api_only = true
 
   end
 end
